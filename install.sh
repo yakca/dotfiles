b3ce12c08742c2d2ee9d2ffd0d5486b45770ae79
Kevin Mackay <mackay.ka@gmail.com>
2015-03-21 18:59:26 +0000

    Add solarized dark theme for qtcreator

--- install.sh
+++ install.sh
@@ -5,4 +5,5 @@ ln -s `pwd`/.vimrc ~/.vimrc
 ln -s `pwd`/zathurarc ~/.config/zathura/zathurarc
 ln -s `pwd`/.i3 ~/.i3
 ln -s `pwd`/.compton.conf ~/.compton.conf
-
+mkdir ~/.config/QtProject/qtcreator/styles
+ln -s `pwd`/solarized-dark.xml ~/.config/QtProject/qtcreator/styles
