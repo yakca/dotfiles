c5ea34612463e3ef2f29f5e604cfee45d6bff0d6
Kevin Mackay <mackay.ka@gmail.com>
2015-03-25 20:33:08 +0000

    Add i3status.conf

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ ln -s `pwd`/.xinitrc ~/.xinitrc
 ln -s `pwd`/.vimrc ~/.vimrc
 ln -s `pwd`/zathurarc ~/.config/zathura/zathurarc
 ln -s `pwd`/.i3 ~/.i3
+ln -s `pwd`/.i3status.conf ~/.i3status.conf
 ln -s `pwd`/.compton.conf ~/.compton.conf
 ln -s `pwd`/.gitconfig ~/.gitconfig
 ln -s `pwd`/.gitignore ~/.gitignore
