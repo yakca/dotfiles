57489912facdafc63c4a157d20deb2c9069f9fb5
Kevin Mackay <mackay.ka@gmail.com>
2015-04-07 22:07:16 +0100

    Rename zshrc to zshrc.local for grml use

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 ln -s `pwd`/.zlogin ~/.zlogin
-ln -s `pwd`/.zshrc ~/.zshrc
+ln -s `pwd`/.zshrc.local ~/.zshrc.local
 ln -s `pwd`/.xinitrc ~/.xinitrc
 ln -s `pwd`/.vimrc ~/.vimrc
 ln -s `pwd`/zathurarc ~/.config/zathura/zathurarc
